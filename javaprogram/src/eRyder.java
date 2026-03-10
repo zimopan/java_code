d06dbb96357f9c3e47096a970a081666e7239497
zimopan <1878592028@qq.com>
2026-03-10 21:03:40 +0800

    2026.3.10

--- javaprogram/src/eRyder.java
+++ javaprogram/src/eRyder.java
@@ -3,12 +3,29 @@ public class eRyder{
     int batteryLevel;
     boolean isAvailable;
     int kmDriven;
+    String COMPANY_NAME;
+    double BASE_FARE;
+    double PER_MINUTE_FARE;
+    private int totalUsageInMinutes;
+    private double totalFare;
+    final String LINKED_ACCOUNT;
+    final int LINKED_PHONE_NUMBER;
+    int  usageInMinutes;
 
     public eRyder(){
         bikeID=123456;
         batteryLevel=0;
         isAvailable=true;
         kmDriven=0;
+        COMPANY_NAME="ERyder";
+        BASE_FARE=1.0;
+        PER_MINUTE_FARE=0.5;
+        totalUsageInMinutes=2;
+        totalFare=30;
+        LINKED_ACCOUNT="mo";
+        LINKED_PHONE_NUMBER=182311;
+        usageInMinutes=300;
+
     }
     public eRyder(int bikeID,int batteryLevel,boolean isAvailable,int kmDriven)
     {
@@ -16,7 +33,12 @@ public class eRyder{
         this.batteryLevel=batteryLevel;
         this.isAvailable=isAvailable;
         this.kmDriven=kmDriven;
+        LINKED_ACCOUNT="mo";
+        LINKED_PHONE_NUMBER=182311;
     }
+
+
+
     
     public boolean ride(){
     if(batteryLevel>=0){
@@ -39,5 +61,16 @@ public class eRyder{
         System.out.println("battery level:"+batteryLevel);
         System.out.println("bike availability:"+isAvailable);
         System.out.println("distance travelled in km:"+kmDriven);
+    }
+    public void printRideDetails(){
+        System.out.println("link account:"+LINKED_ACCOUNT);
+        System.out.println("link phone number:"+LINKED_PHONE_NUMBER);
+        System.out.println("bikeID:"+bikeID);
+        System.out.println("usageInMinutes:"+usageInMinutes);
+        System.out.println("total fare:"+totalFare);
+    }
+    private double calculateFare(){
+        totalFare=BASE_FARE+PER_MINUTE_FARE*usageInMinutes;
+        return totalFare;
     }
 }
--- javaprogram/src/main.java
+++ javaprogram/src/main.java
@@ -5,5 +5,11 @@ eRyder e1=new eRyder();
 e1.printBikeDetails();
 eRyder e2=new eRyder(12345,6,true,200);
 e2.printBikeDetails();
+eRyder e3=new eRyder();
+eRyder e4=new eRyder(12345,6,true,200);
+e3.printBikeDetails();
+e4.printBikeDetails();
+
+
 }
 }
